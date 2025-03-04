652bca4c83b1d882c5a1e409aa4f3d9477563028
Alan DAmico <adamico1@uncc.edu>
2025-03-04 12:27:22 -0500

    Initial Page Setups

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { ReactNode, Suspense } from "react";
 
 export default function UserLayout({
@@ -15,10 +16,47 @@ export default function UserLayout({
 
 function Navbar() {
   return (
-    <header>
-      <nav>
+    <header className="flex h-12 shadow bg-background z-10 ml-3">
+      <nav className="flex gap-4 container">
         <Suspense>
-          <SignedIn></SignedIn>
+          <SignedIn>
+            <Link
+              href="/"
+              className="mr-auto text-lg hover:underline flex items-center"
+            >
+              Home
+            </Link>
+            <Link
+              href="/container"
+              className="hover:bg-accent flex items-center px-2"
+            >
+              Containers
+            </Link>
+            <Link
+              href="/item"
+              className="hover:bg-accent flex items-center px-2"
+            >
+              Items
+            </Link>
+            <Link
+              href="/tag"
+              className="hover:bg-accent flex items-center px-2"
+            >
+              Tags
+            </Link>
+            <Link
+              href="/type"
+              className="hover:bg-accent flex items-center px-2"
+            >
+              Types
+            </Link>
+            <Link
+              href="/settings"
+              className="hover:bg-accent flex items-center px-2"
+            >
+              Settings
+            </Link>
+          </SignedIn>
           <div className="size-8 self-center">
             <UserButton
               appearance={{
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="">Hi</div>;
+  return (
+    <div className="container-6 grid grid-cols-2 sm:grid-cols-2">
+      <div>Container</div>
+      <div>Item</div>
+    </div>
+  );
 }
